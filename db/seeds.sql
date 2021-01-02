734f924a3d9b8813df70b4f8c60b0611321ba9bf
Omair Muhi <omairmuhi@icloud.com>
2021-01-02 09:22:26 -0500

    Subject: Add seeds.sql Problem:NA Solution:NA Note: Insert some records into department table.

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE employee_DB;
+
+/* Insert some departments */
+INSERT INTO department(name)
+VALUES("Engineering");
+
+INSERT INTO department(name)
+VALUES("Sales");
+
+INSERT INTO department(name)
+VALUES("Marketing");
+
